644da87df758d269d298181a939ac99f7a1a027a
Cedar Edwards <daedwards@vmware.com>
2021-05-17 12:26:17 -0400

    Fix my name

--- src/slides/Hello.jsx
+++ src/slides/Hello.jsx
@@ -3,7 +3,7 @@ import { FeatureLine, FeatureSlide } from '../FeatureSlide'
 
 export const Hello = props => (
   <FeatureSlide title="hello!" icon="info-circle">
-    <FeatureLine show={props.index >= 1}>David Edwards</FeatureLine> 
+    <FeatureLine show={props.index >= 1}>Cedar Edwards</FeatureLine>
     <FeatureLine show={props.index >= 2}>engineer, Pivotal Labs</FeatureLine> 
     <FeatureLine show={props.index >= 3}>veteran TDDer</FeatureLine> 
   </FeatureSlide>
